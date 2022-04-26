dbb4a16618fe789b6a3b038f30dffa70869176f7
yinghuiSUN <sunyinghui0506@gmail.com>
2022-04-26 10:14:55 +0200

    ajout de mainTest Class

---
+++ src/main/java/beans/mainTest.java
@@ -0,0 +1,33 @@
+package beans;
+
+import beans.ClientDAO.ClientDAO;
+import beans.ClientDAO.ClientDAOImp;
+import java.util.ArrayList;
+
+public class mainTest {
+    public static void main(String[] args) {
+
+        //tester si la méthode de findAll marche
+        ClientDAO dao1= new ClientDAOImp();
+        ArrayList<Client> list=dao1.findAll();
+
+        for (Client c:list){
+            System.out.println(c);
+        }
+        //ça marche bien
+
+        Client c1=new Client(3, "Paul", "paullll@gmail.com");
+        //le cid, on s'en fiche, car dans la database, c'est généré automatiquement
+        ClientDAO dao2=new ClientDAOImp();
+
+
+        ArrayList<Client> list2=dao2.findAll();
+        for (Client c:list2){
+            System.out.println(c);
+        }
+
+
+
+    }
+
+}
