f36c31607eb85fe5568021d66406c9f37cc7b3de
AlexCFs <AlexCFs>
2022-04-26 17:24:58 +0200

    modif alex

--- src/main/java/beans/Servlet/CreationCompte.java
+++ src/main/java/beans/Servlet/CreationCompte.java
@@ -11,11 +11,23 @@ import java.io.IOException;
 public class CreationCompte extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String n=request.getParameter("name");
+        String n=request.getParameter("nom");
+        String p=request.getParameter("prenom");
+        String t=request.getParameter("telephone");
+        String a=request.getParameter("adresse");
+        String c=request.getParameter("codePostal");
+        String v=request.getParameter("ville");
         String e= request.getParameter("email");
+        String m= request.getParameter("mdp");
 
         request.setAttribute("name" , n);
-        request.setAttribute("email",e);
+        request.setAttribute("prenom" , p);
+        request.setAttribute("telephone" , t);
+        request.setAttribute("adresse" , a);
+        request.setAttribute("codePostal" , c);
+        request.setAttribute("ville" , v);
+        request.setAttribute("email" , e);
+        request.setAttribute("mdp", m);
 
         //envoyer ces donnés vers creationCompte.jsp où les données sont ajoutés dans le tableau
         this.getServletContext().getRequestDispatcher("/creationCompte.jsp").forward(request, response);
