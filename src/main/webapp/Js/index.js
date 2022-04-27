4460af10b2cb304e847215beb111232d74bf3166
mbengndong <smbeng321@gmail.com>
2022-04-27 14:47:00 +0200

    ranger fichier html css et js

--- src/main/webapp/Js/index.js
+++ src/main/webapp/Js/index.js
@@ -1,2 +1,2 @@
 document.getElementById("banniere").innerHTML =
-    "<div><video class='image-banniere' autoplay='autoplay' muted='' loop='infinite'  src='img/VENTE%20DE%20PRODUITS%20EN%20LIGNE.mp4'></video></div>";
+    "<div><video class='image-banniere' autoplay='autoplay' muted='' loop='infinite'  src='../images/VENTE%20DE%20PRODUITS%20EN%20LIGNE.mp4'></video></div>";
