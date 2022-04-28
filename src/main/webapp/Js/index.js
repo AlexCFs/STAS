2270b9fa1250baaa8f21d6f8c6ca5544062a0c5a
mbengndong <smbeng321@gmail.com>
2022-04-28 10:46:08 +0200

    ranger fichier html css et js

--- src/main/webapp/Js/index.js
+++ src/main/webapp/Js/index.js
@@ -2,7 +2,7 @@ document.getElementById("banniere").innerHTML =
     "<div><video class='image-banniere' autoplay='autoplay' muted='' loop='infinite'  src='images/VENTE%20DE%20PRODUITS%20EN%20LIGNE.mp4'></video></div>";
 
 document.getElementById("blocCard").innerHTML =
-    "<div class='flex-column-around card-size card'><h3 class='titre-card'> bebe </h3> </div>"
+    "<div class='flex-column-around card-size card'><<video class='titre-car' autoplay='autoplay' muted='' loop='infinite'  src='image/'></video> </div>";
 document.getElementById("dodo").innerHTML =
     "<div class='flex-column-around card-size card'><h3 class='titre-card'> courage </h3> </div>"
 
